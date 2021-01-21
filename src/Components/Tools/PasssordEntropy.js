d20afcdf5771be47cf315596904a6134456338f3
Tim Cutting <34442834+timcutting-gfm@users.noreply.github.com>
2021-01-21 22:42:32 +0000

    Update

--- src/Components/Tools/PasssordEntropy.js
+++ src/Components/Tools/PasssordEntropy.js
@@ -6,41 +6,21 @@ import PasswordGenerator from './PasswordGenerator';
 import ToolsNav from './ToolsNavigation';
 
 function PasswordEntropy() {
-
-    const [alhpaLower, setAlphaLower] = useState(true);
-    const [alhpaUpper, setAlphaUpper] = useState(true);
-    const [numbers, setNumbers] = useState(true);
-    const [numberSymbols, setNumberSymbols] = useState(true);
-    const [otherSymbols, setOtherSymbols] = useState(true);
     const [password, setPassword] = useState('');
     const [result, setResult] = useState(0);
     const [charSet, setCharSet] = useState(0);
     const [wittyText, setWittyText] = useState(' ');
+    const [includes, setIncludes] = useState({
+        lower: null,
+        upper: null,
+        num: null,
+        csymb: null,
+        osymb: null
+    });
     const Theme = useContext(ThemeContext);
 
     const calcEntropy = (len, chars , pw) => {
-        const crap = [
-            '123456',
-            '123456789',
-            'picture1',
-            'password',
-            '12345678',
-            '111111',
-            '123123',
-            '12345',
-            '1234567890',
-            'senha',
-            '1234567',
-            'qwerty',
-            'abc123',
-            'Million2',
-            '000000',
-            '1234',
-            'iloveyou',
-            'aaron431',
-            'password1',
-            'qqww1122'
-        ];
+        const crap = ['123456','123456789','picture1','password','12345678','111111','123123','12345','1234567890','senha','1234567','qwerty','abc123','Million2','000000','1234','iloveyou','aaron431','password1','qqww1122'];
 
         let bits = parseInt(Math.log2(Math.pow(chars, len)), 10),
             txt = 'So very, very bad';
@@ -59,19 +39,44 @@ function PasswordEntropy() {
     useEffect(() => {
         let chars = 0,
             ret;
-        if(alhpaLower) chars = chars + 26;
-        if(alhpaUpper) chars = chars + 26;
-        if(numbers) chars = chars + 10;
-        if(numberSymbols) chars = chars + 10;
-        if(otherSymbols) chars = chars + 23;
+
+        if(/[a-z]/.test(password)) chars = chars + 26;
+        if(/[A-Z]/.test(password)) chars = chars + 26;
+        if(/[0-9]/.test(password)) chars = chars + 10;
+        if(/[!"£$%^&*()]/.test(password)) chars = chars + 10;
+        if(/[`¬\-=_+[\]{};'#:@~,./<>?\\|]/.test(password)) chars = chars + 23;
 
         ret = calcEntropy(password.length, chars, password);
+
+        setIncludes({
+            lower:  /[a-z]/.test(password),
+            upper:  /[A-Z]/.test(password),
+            num:    /[0-9]/.test(password),
+            csymb:  /[!"£$%^&*()]/.test(password),
+            osymb:  /[`¬\-=_+[\]{};'#:@~,./<>?\\|]/.test(password)
+        });
+        
+        if(password.length < 1) {
+            setIncludes({
+                lower: null,
+                upper: null,
+                num: null,
+                csymb: null,
+                osymb: null
+            });
+        }
 
         setResult(ret.res);
         setWittyText(ret.strength)
         setCharSet(chars);
 
-    }, [alhpaLower, alhpaUpper, numberSymbols, numbers, otherSymbols, password]);
+    }, [password]);
+
+    const charIcon = (v) => {
+        if(v === null) return 'double-dash text-mid';
+        if(v === true) return 'tick-circle text-success';
+        return 'attention text-warning';
+    }
 
     document.title = 'Password Strength Test - Calculate your password entropy';
 
@@ -81,68 +86,9 @@ function PasswordEntropy() {
             <ToolsNav />
             <Container as="section">
                 <Row xs={1} lg={3}>
-                    <Col className="mb-5 mb-lg-0">
-                        <h2 className="mb-3">
-                            <span className="d-flex align-items-center">
-                                 Character Set
-                                <small className="text-muted ml-2">{charSet}</small>
-                                <OverlayTrigger trigger="click" placement="bottom" overlay={
-                                    <Popover className={`shadow${Theme === 'dark' ? ' dark-mode' : ''}`}>
-                                        <Popover.Title as="h2" className="text-capitalize p-3">Character Set</Popover.Title>
-                                        <Popover.Content className="p-3">
-                                    Sometimes you will have rules imposed on you about which characters you can use.  Usually these are upper and lowercase letters, numbers and some symobls.  You can choose to limit the character set with these options to better reflect the strength of the password.
-                                        </Popover.Content>
-                                    </Popover>
-                                }>
-                                    <i className="icon-info text-primary size-2 cursor-pointer ml-auto"></i>           
-                                </OverlayTrigger>
-                            </span>
-                           
-                        </h2>
-                        <div className="d-flex align-items-start mt-4">
-                            <div>
-                                <Form.Group controlId="al-switch" className="m-0">
-                                    <Form.Switch 
-                                        label="Lowercase Letters"
-                                        checked={alhpaLower}
-                                        onChange={() => setAlphaLower(!alhpaLower)}
-                                    />
-                                </Form.Group>
-                                <Form.Group controlId="au-switch" className="m-0">
-                                    <Form.Switch 
-                                        label="Uppercase Letters"
-                                        checked={alhpaUpper}
-                                        onChange={() => setAlphaUpper(!alhpaUpper)}
-                                    />
-                                </Form.Group>
-                                <Form.Group controlId="n-switch" className="m-0">
-                                    <Form.Switch 
-                                        label="Numbers"
-                                        checked={numbers}
-                                        onChange={() => setNumbers(!numbers)}
-                                    />
-                                </Form.Group>
-                                <Form.Group controlId="ns-switch" className="m-0">
-                                    <Form.Switch 
-                                        label="Number Symbols"
-                                        checked={numberSymbols}
-                                        onChange={() => setNumberSymbols(!numberSymbols)}
-                                    />
-                                </Form.Group>
-                                <Form.Group controlId="os-switch" className="m-0">
-                                    <Form.Switch 
-                                        label="Other Symbols"
-                                        checked={otherSymbols}
-                                        onChange={() => setOtherSymbols(!otherSymbols)}
-                                    />
-                                </Form.Group>
-                            </div>
-                        </div>
-                     
-                    </Col>
-                    <Col className="mb-5 mb-lg-0">
-                        <h2>Password</h2>
-                        <Form.Group controlId="formBasicEmail">
+                    <Col>
+                        <h2>Type a Password</h2>
+                        <Form.Group controlId="password">
                             <Form.Label>Enter a password example</Form.Label>
                             <Form.Control
                                 type="password"
@@ -154,13 +100,58 @@ function PasswordEntropy() {
                             This will not be sent anywhere.
                             </Form.Text>
                         </Form.Group>
+                        <p className="text-muted">Start typing a password to see its entropy score and related insights.</p>
                     </Col>
-                    <Col className="mb-5 mb-lg-0">
+                    <Col>
+                        <h2 className="mb-3">
+                            <span className="d-flex align-items-center">
+                                &nbsp;
+                                <OverlayTrigger trigger="click" placement="bottom" overlay={
+                                    <Popover className={`shadow${Theme === 'dark' ? ' dark-mode' : ''}`}>
+                                        <Popover.Title as="h2" className="text-capitalize p-3">Character Set</Popover.Title>
+                                        <Popover.Content className="p-3">
+                                            These numbers indicate the total character set based on the types of characters used in the password.  Use a range of characters to increase the subset of characters, which in turn increases the calculated entropy.
+                                        </Popover.Content>
+                                    </Popover>
+                                }>
+                                    <i className="icon-info text-primary size-2 cursor-pointer ml-auto mr-5"></i>           
+                                </OverlayTrigger>
+                            </span>
+                        </h2>
+                        
+                        <p className="mb-2">
+                            <i className={`icon-${charIcon(includes.lower)} mr-2`}></i>
+                            Lowercase Letters
+                            <small className="text-muted float-right mt-1 mr-5">26 chars</small>
+                        </p>
+                        <p className="mb-2">
+                            <i className={`icon-${charIcon(includes.upper)} mr-2`}></i>
+                            Uppercase Letters
+                            <small className="text-muted float-right mt-1 mr-5">26 chars</small>
+                        </p>
+                        <p className="mb-2">
+                            <i className={`icon-${charIcon(includes.num)} mr-2`}></i>
+                            Numbers
+                            <small className="text-muted float-right mt-1 mr-5">10 chars</small>
+                        </p>
+                        <p className="mb-2">
+                            <i className={`icon-${charIcon(includes.csymb)} mr-2`}></i>
+                            Shift + <kbd>[0-9]</kbd> Symbols
+                            <small className="text-muted float-right mt-1 mr-5">10 chars</small>
+                        </p>
+                        <p className="mb-2">
+                            <i className={`icon-${charIcon(includes.osymb)} mr-2`}></i>
+                            Other Symbols
+                            <small className="text-muted float-right mt-1 mr-5">23 chars</small>
+                        </p>
+                        <p>
+                            <small className="text-muted float-right mt-1 mr-5">Totol {charSet} chars</small>
+                        </p>
+                    </Col>
+                    <Col>
                         <h2>Result</h2>
                         <p className="mb-2">{result === '💩' ? 'This password is too common' : (wittyText === ' ' ? '\u00A0' : wittyText)}</p>
-                        {password.length === 0 ? (
-                            <p className="text-muted">Enter a password to see a result.</p>
-                        ) : (
+                        {password.length !== 0 && (
                             <p className="h1 text-primary text-capitalize">
                                 {result === '💩' ? result : `~${result} Bits`}
                             </p>
