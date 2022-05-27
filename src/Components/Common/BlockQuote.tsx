1b547d08eba26a63cd6f01397c195185f7878b53
Tim Cutting <34442834+timcutting-gfm@users.noreply.github.com>
2022-05-27 08:02:56 +0100

    Update blockquote comp

--- src/Components/Common/BlockQuote.tsx
+++ src/Components/Common/BlockQuote.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 import { Button, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-function BlockQuote({content, fluid, icon, buttons}) {
+type Props = {
+    content: string[],
+    fluid: boolean,
+    icon: string,
+    buttons: {
+        icon: string,
+        label: string,
+        href: string,
+        type: string
+    }[]
+}
+
+function BlockQuote({content, fluid, icon, buttons}: Props) {
     return (
         <Container fluid={fluid}>
             <blockquote className="blockquote d-block d-md-flex">
