c41fed5b26dfecb6a72efa14c860a58473dae0ca
Tim Cutting <idlelimey@outlook.com>
2022-06-29 08:27:56 +0100

    Improve readability of tool card links

--- src/Components/Tools/ToolCard.js
+++ src/Components/Tools/ToolCard.js
@@ -1,7 +1,10 @@
+import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { ThemeContext } from '../../App';
 import PropTypes from 'prop-types';
 
 const ToolCard = ({to, size, icon, title, description}) => {
+    const Theme = useContext(ThemeContext);
     return (
         <NavLink
             to={to}
@@ -10,7 +13,7 @@ const ToolCard = ({to, size, icon, title, description}) => {
             <i className={`icon-${icon} size-5 font-weight-normal`}></i>
             <p className="h3">{title}</p>
             {description && (
-                <p className='text-mid mb-0'>{description}</p>
+                <p className={`text-${Theme === 'dark' ? 'lighter' : 'dark'} mb-0`}>{description}</p>
             )}
         </NavLink>
     );
