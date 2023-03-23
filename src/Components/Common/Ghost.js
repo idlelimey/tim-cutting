2ec5fbdebb7d7eb6bd967fe235bca01c29062617
idlelimey <idlelimey@outlook.com>
2023-03-23 12:08:31 +0000

    Update Ghost.js

--- src/Components/Common/Ghost.js
+++ src/Components/Common/Ghost.js
@@ -2,10 +2,10 @@ import { useEffect, useRef } from 'react';
 
 function Ghost() {
     const ghost = useRef(null);
+    const blur = useRef(null);
     useEffect(() => {
         document.body.onpointermove = (e) => {
             const { pageX, pageY } = e;
-            //console.info('e', e);
             ghost.current.animate(
                 {
                     left: `${pageX - 250}px`,
@@ -13,14 +13,15 @@ function Ghost() {
                 },
                 { duration: 5000, fill: 'forwards' }
             );
-            //console.info('xy', x, y);
+            // Fix bug where blur needs to be reset on window resize by re-applying rule.
+            blur.current.style.backdropFilter = 'blur(200px)';
         };
     }, []);
 
     return (
         <>
             <div id='ghost' ref={ghost}></div>
-            <div id='blur'></div>
+            <div id='blur' ref={blur}></div>
         </>
     );
 }
