0197397e6f06618bd204513836a50aa0a44919e0
idlelimey <idlelimey@outlook.com>
2023-03-23 08:30:28 +0000

    Update Content.js

--- src/Data/Content.js
+++ src/Data/Content.js
@@ -9,7 +9,8 @@ export const tools = [
         title: 'CSS Gradient Generator',
         icon: 'code',
         to: 'css-gradient-generator',
-        description: 'Build a multi-layer CSS gradient easily with this generator. Modify the layers gradient angle and set colour stops, you can set the visibility of each layer to make it easier to see what is going on with individual layers.',
+        description:
+            'Build a multi-layer CSS gradient easily with this generator. Modify the layers gradient angle and set colour stops, you can set the visibility of each layer to make it easier to see what is going on with individual layers.',
         size: 2,
     },
     {
@@ -22,7 +23,7 @@ export const tools = [
         title: 'Password Entropy',
         icon: 'password',
         to: 'password-entropy',
-        description: 'Don\'t use bad passwords, use this tester and generator and use better passwords.',
+        description: "Don't use bad passwords, use this tester and generator and use better passwords.", // eslint-disable-line
     },
     {
         title: 'Your Weight In...',
@@ -33,111 +34,97 @@ export const tools = [
 ];
 
 export const blockquotes = {
-    'top': [
+    top: [
         'I am a seasoned and enthusiastic web developer with a number of essential and valuable digital skills from many years in development, design and marketing.',
-        'I have a strong background in commercial and boutique web development using standards-led JavaScript, SASS and PHP to produce high quality design & data driven user experiences within many frameworks including React, WordPress and native platforms.'
+        'I have a strong background in commercial and boutique web development using standards-led JavaScript, SASS and PHP to produce high quality design & data driven user experiences within many frameworks including React, WordPress and native platforms.',
     ],
-    'bottom': [
-        'Years of experience and the skills necessary to produce modern front-end applications from conception and design through to development, testing and deployment.'
-    ]
+    bottom: [
+        'Years of experience and the skills necessary to produce modern front-end applications from conception and design through to development, testing and deployment.',
+    ],
 };
 
 export const icons = [
-    {'icon': 'javascript', 'text': 'JavaScript'},
-    {'icon': 'react', 'text': 'React'},
-    {'icon': 'wordpress', 'text': 'Wordpress'},
-    {'icon': 'sass', 'text': 'SASS/SCSS'},
-    {'icon': 'html', 'text': 'HTML5'},
-    {'icon': 'booststrap', 'text': 'Bootstrap'},
-    {'icon': 'github', 'text': 'Git/GitHub'},
-    {'icon': 'vscode', 'text': 'Visual Studio Code'},
-    {'icon': 'adobe-cc', 'text': 'Adobe Creative Cloud'}
+    { icon: 'javascript', text: 'JavaScript' },
+    { icon: 'react', text: 'React' },
+    { icon: 'wordpress', text: 'Wordpress' },
+    { icon: 'sass', text: 'SASS/SCSS' },
+    { icon: 'html', text: 'HTML5' },
+    { icon: 'booststrap', text: 'Bootstrap' },
+    { icon: 'github', text: 'Git/GitHub' },
+    { icon: 'vscode', text: 'Visual Studio Code' },
+    { icon: 'adobe-cc', text: 'Adobe Creative Cloud' },
 ];
 
 export const jobs = [
     {
-        'order': 3,
-        'title': 'Webmaster, Design and Development',
-        'company': 'Laptopshop IT Ltd.',
-        'period': '2003 - 2009',
-        'content': [
-            'My role as a Web master took me from maintaining thousands of products for a busy online Laptop retailer to providing a full web master role for a constantly developing bespoke site (PHP, MySQL, JS).  As well as the technical role of both front and back-end development I was solely responsible for all design assignments - banners, email campaigns and product presentation.'
-        ]
+        order: 3,
+        title: 'Webmaster, Design and Development',
+        company: 'Laptopshop IT Ltd.',
+        period: '2003 - 2009',
+        content: [
+            'My role as a Web master took me from maintaining thousands of products for a busy online Laptop retailer to providing a full web master role for a constantly developing bespoke site (PHP, MySQL, JS).  As well as the technical role of both front and back-end development I was solely responsible for all design assignments - banners, email campaigns and product presentation.',
+        ],
     },
     {
-        'order': 2,
-        'title': 'SEO, Design and Development',
-        'company': 'High Position Ltd.',
-        'period': '2009 - 2011',
-        'content': [
-            'My role as an SEO specialist and Developer took me from working with small businesses to major global corporations such as Sony (Europe). I was able to support the companies to increase revenue and acquire more customers by leading assignments that focused on the provision of UX and conversion rate optimisation proposals to maximise visitor engagement and to tie in the SEO traffic with greater conversions through usability studies.'
-        ]
+        order: 2,
+        title: 'SEO, Design and Development',
+        company: 'High Position Ltd.',
+        period: '2009 - 2011',
+        content: [
+            'My role as an SEO specialist and Developer took me from working with small businesses to major global corporations such as Sony (Europe). I was able to support the companies to increase revenue and acquire more customers by leading assignments that focused on the provision of UX and conversion rate optimisation proposals to maximise visitor engagement and to tie in the SEO traffic with greater conversions through usability studies.',
+        ],
     },
     {
-        'order': 1,
-        'title': 'Marketing, Design and Development',
-        'company': 'Sports Supplements Ltd.',
-        'period': '2011 - 2016',
-        'content': [
+        order: 1,
+        title: 'Marketing, Design and Development',
+        company: 'Sports Supplements Ltd.',
+        period: '2011 - 2016',
+        content: [
             'I initially joined the company as the sole Developer and Designer, part of a new marketing team, tasked to take the businesses from simple mail shots to modern marketing using a variety of tools to reach more customers in a better way.',
-            'As the site base grew, I moved entirely to development where I facilitated the roll-out of European sites and numerous feature enhancements. I created and led an in-house photography suite for pack shooting.'
-        ]
+            'As the site base grew, I moved entirely to development where I facilitated the roll-out of European sites and numerous feature enhancements. I created and led an in-house photography suite for pack shooting.',
+        ],
     },
     {
-        'order': 0,
-        'title': 'Senior Frontend Developer',
-        'company': 'GFM, Unit 6',
-        'period': '2017 - 2022',
-        'content': [
+        order: 0,
+        title: 'Senior Frontend Developer',
+        company: 'GFM, Unit 6',
+        period: '2017 - 2022',
+        content: [
             'I joined the company initially to focus on driving a new email marketing platform as a value add for clients as well as to assist in maintenance of key clients such as the Sun and the Daily Mail.',
-            'Before long I took the frontend developer role for the Telegraph Fantasy Football games, which further expanded in to versions for Rugby, Cricket & Horse Racing.  I then became the lead Developer for the frontend team to work on a React rebuild of a Holidays application within the business.'
-        ]
-    }
+            'Before long I took the frontend developer role for the Telegraph Fantasy Football games, which further expanded in to versions for Rugby, Cricket & Horse Racing.  I then became the lead Developer for the frontend team to work on a React rebuild of a Holidays application within the business.',
+        ],
+    },
 ];
 
 export const infoCards = [
     {
-        'icon': 'code',
-        'heading': 'Frontend',
-        'subheading': 'Front and centre',
-        'content': 'Modern frontend web development using the latest technology to drive performance and scalability.',
-        'list': [
-            'JavaScript, React',
-            'Responsive',
-            'WordPress'
-        ],
+        icon: 'code',
+        heading: 'Frontend',
+        subheading: 'Front and centre',
+        content: 'Modern frontend web development using the latest technology to drive performance and scalability.',
+        list: ['JavaScript, React', 'Responsive', 'WordPress'],
     },
     {
-        'icon': 'paint',
-        'heading': 'Design',
-        'subheading': 'Modern, Useful',
-        'content': 'Attractive and usable design combining the best UX/UI and design practices with browser compatibility.',
-        'list': [
-            'User Experience',
-            'Web & Packaging Design',
-            'Branding'
-        ],
+        icon: 'paint',
+        heading: 'Design',
+        subheading: 'Modern, useful',
+        content:
+            'Attractive and usable design combining the best UX/UI and design practices with browser compatibility.',
+        list: ['User Experience', 'Web & Packaging Design', 'Branding'],
     },
     {
-        'icon': 'seo',
-        'heading': 'Search Optimised',
-        'subheading': 'Human readable',
-        'content': 'Delivering SEO compliant code with an emphasis on maintaining readability for your end users.',
-        'list': [
-            'AMP Development',
-            'Page Optimisation',
-            'JSON-LD Schema'
-        ],
+        icon: 'seo',
+        heading: 'Search optimised',
+        subheading: 'Human readable',
+        content: 'Delivering SEO compliant code with an emphasis on maintaining readability for your end users.',
+        list: ['AMP Development', 'Page Optimisation', 'JSON-LD Schema'],
     },
     {
-        'icon': 'no-bug',
-        'heading': 'Compatibility',
-        'subheading': 'Across Every Device',
-        'content': 'Browser & device tested for compatibility.  Strict accessibility testing ensures  content is rendered the way you intended.',
-        'list': [
-            'Device testing',
-            'Full Browser Compatibility',
-            'WCAG Compliancy'
-        ],
+        icon: 'no-bug',
+        heading: 'Compatibility',
+        subheading: 'Across every device',
+        content:
+            'Browser & device tested for compatibility.  Strict accessibility testing ensures  content is rendered the way you intended.',
+        list: ['Device testing', 'Full Browser Compatibility', 'WCAG Compliancy'],
     },
-]
+];
