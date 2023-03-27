53495c1ae6fd6f07f22249bc058b36f18c23d614
idlelimey <idlelimey@outlook.com>
2023-03-27 15:11:43 +0100

    Add logos to content data

--- src/Components/Common/InfoCard.js
+++ src/Components/Common/InfoCard.js
@@ -6,21 +6,28 @@ function InfoCard(props) {
     const Theme = useContext(ThemeContext);
 
     return (
-        <Col className="mb-5 d-flex info-card">
-            <div className="flex-grow-0">
-                <i className={`icon-${props.icon} size-5 mr-3 text-${Theme === 'dark' ? 'mid' : 'lighter'}`} style={{lineHeight: '1'}}></i>
+        <Col className='mb-5 d-flex info-card' data-aos={props.aosType} data-aos-delay={props.aosDelay}>
+            <div className='flex-grow-0'>
+                <i
+                    className={`icon-${props.icon} size-5 mr-3 text-${Theme === 'dark' ? 'mid' : 'lighter'}`}
+                    style={{ lineHeight: '1' }}
+                ></i>
             </div>
-            <div className="flex-grow-1">
-                <h1 className="mb-3">{props.heading}<br /><small className="text-muted">{props.subheading}</small></h1>
-                <p className="lead">{props.content}</p>
-                <ul className="ticks">
-                    {props.list.map((l,i) => 
+            <div className='flex-grow-1'>
+                <h1 className='mb-3'>
+                    {props.heading}
+                    <br />
+                    <small className='text-muted'>{props.subheading}</small>
+                </h1>
+                <p className='lead'>{props.content}</p>
+                <ul className='ticks'>
+                    {props.list.map((l, i) => (
                         <li key={i}>{l}</li>
-                    )}
+                    ))}
                 </ul>
             </div>
         </Col>
-    )
+    );
 }
 
 export default InfoCard;
